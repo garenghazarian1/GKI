20aaceb6493ebb3f68f751fd91366513eb33a526
Garen Ghazarian winmsi <garenghazarian1@gmail.com>
2023-11-27 10:13:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+    <h1 className="text-3xl text-red-600 font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+ 
