ff4c64a09cd72f0e9df0a595899054631976eecd
KostasBzn <kostasbouzianis@gmail.com>
2023-11-28 14:33:02 +0100

    Reserved cars components

--- src/navbar-pages/Dashboard.jsx
+++ src/navbar-pages/Dashboard.jsx
@@ -1,8 +1,13 @@
+import ReservedCars from "./ReservedCars";
+
 export default function Dashboard() {
   return (
     <div className="bg-red-900 h-screen p-8 flex flex-col justify-center align-middle text-center">
       <h1 className="text-3xl font-bold text-gray-200 mb-4">Dashboard</h1>
-      <p className="text-gray-300">Welcome to the Dashboard. Here you can manage your settings and data.</p>
+      <p className="text-gray-300">
+        Welcome to the Dashboard. Here you can manage your settings and data.
+      </p>
+      <ReservedCars />
     </div>
-  )
+  );
 }
